11b5592cba893eeba0ef747a85a1c10286fbe1e1
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-03-19 14:40:33 +0530

    Create Countlines.java

---
+++ Countlines.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+class Countlines
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+	int count=1;
+	while(sc.hasNextLine())
+	{
+            String str =sc.next();
+            int n=str.length();
+            char ch[]=str.toCharArray();
+            for(int j=0;j<n;j++)
+            {
+            	if(ch[j]=='.')
+            	{
+                	j++;
+                	count=count+1;
+            	}
+            }
+	}
+        System.out.println(count);
+    }
+}
