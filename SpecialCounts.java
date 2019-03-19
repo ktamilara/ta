76352b8d90db9ee2b6e0c7141e34c9108ce81d01
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-03-19 14:31:44 +0530

    Create SpecialCounts.java

---
+++ SpecialCounts.java
@@ -0,0 +1,25 @@
+mport java.io.*;
+class SpecialCounts
+{
+public static void main(String ser[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+String s=br.readLine();
+int i,l,c1=0,c2=0,c3=0,sp=0;
+char ch;
+l=s.length();
+for(i=0;i<l;i++)
+{
+ch=s.charAt(i);
+if(Character.isLetter(ch))
+++c1;
+else if(Character.isDigit(ch))
+++c2;
+else if(ch==' ')
+++sp;
+else
+++c3;
+}
+System.out.println(c3);
+}
+}
